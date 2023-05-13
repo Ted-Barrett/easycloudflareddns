91d2a4e3ee7681d272f01f94c4b376def348958f
Ted Barrett <21516680+Ted-Barrett@users.noreply.github.com>
2023-05-13 22:33:38 +1000

    allow installer to be piped to bash

---
+++ README.md
@@ -0,0 +1,15 @@
+Installation:
+
+Make a new directory, for example:
+```
+mkdir easycloudflareddns
+```
+Enter that directory
+```
+cd easycloudflareddns
+```
+
+Run the installer script:
+```
+curl -L https://raw.githubusercontent.com/Ted-Barrett/easycloudflareddns/main/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if [ ! -z "$(ls -A ./)" ]; then
     echo -n "WARNING: Directory not empty. Continue? [Y/n] "
-    read RUN_NOT_EMPTY
+    read RUN_NOT_EMPTY <&1
 
     if !([ -z $RUN_NOT_EMPTY ] || [ $RUN_NOT_EMPTY = "y" ] || [ $RUN_NOT_EMPTY = "Y" ]); then
         echo "Stopping."
@@ -12,10 +12,10 @@ wget https://github.com/Ted-Barrett/easycloudflareddns/archive/main.tar.gz
 tar -xf main.tar.gz --strip-components 1 && rm main.tar.gz
 
 echo -n "Would you like to run the setup now? [Y/n] "
-read RUNSETUP
+read RUNSETUP <&1
 
 if [ -z $RUNSETUP ] || [ $RUNSETUP = "y" ] || [ $RUNSETUP = "Y" ]; then
-    python3 createconfig.py
+    python3 createconfig.py <&1
 else
     echo "Stopping."
 fi
